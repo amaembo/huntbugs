59ee70a3517dd3aa7558193ab90064d7b525bead
lan <lan@developmentontheedge.com>
2016-04-06 17:29:04 +0600

    FloatingPointNaN fixed

--- huntbugs/src/main/java/one/util/huntbugs/detect/FloatingPointNaN.java
+++ huntbugs/src/main/java/one/util/huntbugs/detect/FloatingPointNaN.java
@@ -21,7 +21,7 @@ import com.strobel.decompiler.ast.Expression;
 import com.strobel.decompiler.ast.Node;
 
 import one.util.huntbugs.registry.MethodContext;
-import one.util.huntbugs.registry.anno.AstNodeVisitor;
+import one.util.huntbugs.registry.anno.AstExpressionVisitor;
 import one.util.huntbugs.registry.anno.WarningDefinition;
 import one.util.huntbugs.util.Nodes;
 import one.util.huntbugs.warning.WarningAnnotation;
@@ -32,7 +32,7 @@ import one.util.huntbugs.warning.WarningAnnotation;
  */
 @WarningDefinition(category="Correctness", name="FloatCompareToNaN", baseScore = 90)
 public class FloatingPointNaN {
-    @AstNodeVisitor
+    @AstExpressionVisitor
     public void visit(Expression node, MethodContext ctx) {
         if(Nodes.isComparison(node)) {
             List<Expression> args = node.getArguments();
