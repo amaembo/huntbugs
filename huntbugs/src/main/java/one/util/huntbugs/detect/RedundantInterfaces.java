29a775447b93ab446947f6b14e0887c270b911f9
Tagir Valeev <lany@ngs.ru>
2016-04-17 19:09:37 +0600

    RedundantInterfaces

---
+++ huntbugs/src/main/java/one/util/huntbugs/detect/RedundantInterfaces.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2015, 2016 Tagir Valeev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.detect;
+
+import com.strobel.assembler.metadata.TypeDefinition;
+import com.strobel.assembler.metadata.TypeReference;
+
+import one.util.huntbugs.registry.ClassContext;
+import one.util.huntbugs.registry.anno.ClassVisitor;
+import one.util.huntbugs.registry.anno.WarningDefinition;
+import one.util.huntbugs.util.Types;
+import one.util.huntbugs.warning.WarningAnnotation;
+
+/**
+ * @author lan
+ *
+ */
+@WarningDefinition(category = "CodeStyle", name = "RedundantInterface", maxScore = 20)
+public class RedundantInterfaces {
+    @ClassVisitor
+    public void visit(TypeDefinition td, ClassContext cc) {
+        TypeDefinition baseType = td.getBaseType().resolve();
+        if(baseType == null || baseType.getInternalName().equals("java/lang/Object"))
+            return;
+        for(TypeReference tr : td.getExplicitInterfaces()) {
+            if(tr.getInternalName().equals("java/io/Serializable")) {
+                continue;
+            }
+            if(Types.isInstance(baseType, tr)) {
+                cc.report("RedundantInterface", td.isPublic() ? 0 : 10, WarningAnnotation.forType("INTERFACE", tr));
+            }
+        }
+    }
+}
---
+++ huntbugs/src/test/java/one/util/huntbugs/testdata/TestRedundantInterfaces.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2015, 2016 Tagir Valeev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.testdata;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.RandomAccess;
+import java.util.Set;
+import java.util.TreeSet;
+
+import one.util.huntbugs.registry.anno.AssertNoWarning;
+import one.util.huntbugs.registry.anno.AssertWarning;
+
+/**
+ * @author lan
+ *
+ */
+@AssertWarning(type="RedundantInterface")
+public class TestRedundantInterfaces extends TreeSet<String> implements Set<String>, Collection<String> {
+    private static final long serialVersionUID = 1L;
+
+    @AssertNoWarning(type="RedundantInterface")
+    public static class NoRedundantInterfaces extends LinkedList<String> implements RandomAccess, Serializable {
+        private static final long serialVersionUID = 1L;
+    }
+    
+    @AssertNoWarning(type="RedundantInterface")
+    public static class NoRedundantInterfaces2 extends NoRedundantInterfaces implements Serializable {
+        private static final long serialVersionUID = 1L;
+    }
+}
