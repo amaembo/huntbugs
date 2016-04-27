ff9d57423a1e727d296ea8e1501183105cd67d08
Tagir Valeev <lany@ngs.ru>
2016-04-27 10:19:46 +0300

    EmptySync: support javac

--- huntbugs/src/main/java/one/util/huntbugs/detect/EmptySync.java
+++ huntbugs/src/main/java/one/util/huntbugs/detect/EmptySync.java
@@ -20,6 +20,8 @@ import java.util.List;
 import com.strobel.decompiler.ast.AstCode;
 import com.strobel.decompiler.ast.Block;
 import com.strobel.decompiler.ast.Node;
+import com.strobel.decompiler.ast.TryCatchBlock;
+
 import one.util.huntbugs.registry.MethodContext;
 import one.util.huntbugs.registry.anno.AstVisitor;
 import one.util.huntbugs.registry.anno.WarningDefinition;
@@ -36,9 +38,21 @@ public class EmptySync {
         if(node instanceof Block) {
             List<Node> body = ((Block) node).getBody();
             for(int i=0; i<body.size()-1; i++) {
-                if(Nodes.isOp(body.get(i), AstCode.MonitorEnter)
-                        && Nodes.isOp(body.get(i+1), AstCode.MonitorExit)) {
-                    mc.report("EmptySynchronizeBlock", 0, body.get(i));
+                if(Nodes.isOp(body.get(i), AstCode.MonitorEnter)) {
+                    Node next = body.get(i+1);
+                    if(Nodes.isOp(next, AstCode.MonitorExit))
+                        // ECJ scenario: simply monitorenter/monitorexit
+                        mc.report("EmptySynchronizeBlock", 0, body.get(i));
+                    else if(next instanceof TryCatchBlock) {
+                        TryCatchBlock tryCatch = (TryCatchBlock)next;
+                        if(tryCatch.getCatchBlocks().isEmpty() && tryCatch.getTryBlock().getBody().isEmpty()) {
+                            Block finallyBlock = tryCatch.getFinallyBlock();
+                            if(finallyBlock.getBody().size() == 1 && Nodes.isOp(finallyBlock.getBody().get(0), AstCode.MonitorExit)) {
+                                // JAVAC scenario: try-catch added
+                                mc.report("EmptySynchronizeBlock", 0, body.get(i));
+                            }
+                        }
+                    }
                 }
             }
         }
