6f41ce9b56ac20464f4d7ee140960819cb469cc9
Tagir Valeev <lany@ngs.ru>
2016-05-01 17:18:34 +0600

    AverageComputationCouldOverflow

---
+++ huntbugs/src/main/java/one/util/huntbugs/detect/AverageComputation.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015, 2016 Tagir Valeev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.detect;
+
+import com.strobel.decompiler.ast.AstCode;
+import com.strobel.decompiler.ast.Expression;
+
+import one.util.huntbugs.registry.MethodContext;
+import one.util.huntbugs.registry.anno.AstNodes;
+import one.util.huntbugs.registry.anno.AstVisitor;
+import one.util.huntbugs.registry.anno.WarningDefinition;
+import one.util.huntbugs.util.Nodes;
+
+/**
+ * @author lan
+ *
+ */
+@WarningDefinition(category = "BadPractice", name = "AverageComputationCouldOverflow", maxScore = 35)
+public class AverageComputation {
+    @AstVisitor(nodes = AstNodes.EXPRESSIONS)
+    public void visitExpression(Expression expr, MethodContext mc) {
+        if (expr.getCode() != AstCode.LoadElement && expr.getCode() != AstCode.StoreElement)
+            return;
+        Expression idx = Nodes.getChild(expr, 1);
+        if (!isDiv2(idx))
+            return;
+        Expression orig = Nodes.getChild(idx, 0);
+        if (orig.getCode() != AstCode.Add)
+            return;
+        mc.report("AverageComputationCouldOverflow", 0, idx);
+    }
+
+    private boolean isDiv2(Expression idx) {
+        return (idx.getCode() == AstCode.Div && Integer.valueOf(2).equals(Nodes.getConstant(idx.getArguments().get(1))))
+            || (idx.getCode() == AstCode.Shr && Integer.valueOf(1).equals(Nodes.getConstant(idx.getArguments().get(1))));
+    }
+}
---
+++ huntbugs/src/test/java/one/util/huntbugs/testdata/TestAverageComputation.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015, 2016 Tagir Valeev
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.testdata;
+
+import one.util.huntbugs.registry.anno.AssertNoWarning;
+import one.util.huntbugs.registry.anno.AssertWarning;
+
+/**
+ * @author lan
+ *
+ */
+public class TestAverageComputation {
+    @AssertWarning(type = "AverageComputationCouldOverflow")
+    private static int bsearch(int[] a, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) >> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    @AssertWarning(type = "AverageComputationCouldOverflow")
+    private static int bsearch2(int[] a, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+    @AssertNoWarning(type = "AverageComputationCouldOverflow")
+    private static int bsearchOk(int[] a, int low, int high, int key) {
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                low = mid + 1;
+            else if (midVal > key)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
+
+}
