bd03316892a386b3459a94b0ac124ac4e2a563f6
Tagir Valeev <lany@ngs.ru>
2016-05-27 15:22:23 +0600

    DuplicateAssignment

---
+++ huntbugs/src/main/java/one/util/huntbugs/detect/DuplicateAssignment.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2016 HuntBugs contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.detect;
+
+import java.util.List;
+
+import com.strobel.assembler.metadata.FieldReference;
+import com.strobel.assembler.metadata.MethodDefinition;
+import com.strobel.decompiler.ast.AstCode;
+import com.strobel.decompiler.ast.Block;
+import com.strobel.decompiler.ast.Expression;
+import com.strobel.decompiler.ast.Node;
+
+import one.util.huntbugs.registry.MethodContext;
+import one.util.huntbugs.registry.anno.AstVisitor;
+import one.util.huntbugs.registry.anno.WarningDefinition;
+import one.util.huntbugs.util.NodeChain;
+import one.util.huntbugs.util.Nodes;
+import one.util.huntbugs.warning.Role.LocationRole;
+
+/**
+ * @author lan
+ *
+ */
+@WarningDefinition(category="RedundantCode", name="FieldDoubleAssignment", maxScore=60)
+public class DuplicateAssignment {
+    private static final LocationRole DUPLICATE_ASSIGNMENT_AT = LocationRole.forName("DUPLICATE_ASSIGNMENT_AT");
+    
+    @AstVisitor
+    public void visit(Node node, NodeChain nc, MethodDefinition md, MethodContext mc) {
+        if(node instanceof Block) {
+            List<Node> body = ((Block) node).getBody();
+            for(int i=0; i<body.size()-1; i++) {
+                Node n = body.get(i);
+                if(!(n instanceof Expression))
+                    continue;
+                Expression e = (Expression) n;
+                if(e.getCode() == AstCode.PutField) {
+                    Expression receiver = e.getArguments().get(0);
+                    FieldReference fr = (FieldReference) e.getOperand();
+                    for(int j=i+1; j<body.size(); j++) {
+                        Node n2 = body.get(j);
+                        if(!(n2 instanceof Expression))
+                            break;
+                        Expression e2 = (Expression) n2;
+                        if(e2.getCode() == AstCode.Store || e2.getCode() == AstCode.StoreElement) {
+                            if(Nodes.findExpression(e2, e1 -> !Nodes.isSideEffectFree(e1)) != null)
+                                break;
+                            continue;
+                        }
+                        if(e2.getCode() != AstCode.PutField)
+                            break;
+                        if(!Nodes.isSideEffectFree(e2.getArguments().get(1)))
+                            break;
+                        Expression receiver2 = e2.getArguments().get(0);
+                        FieldReference fr2 = (FieldReference) e2.getOperand();
+                        if(fr.isEquivalentTo(fr2) && Nodes.isEquivalent(receiver, receiver2)) {
+                            int priority = 0;
+                            if(md.isConstructor() && nc == null && Nodes.isThis(receiver2))
+                                continue;
+                            mc.report("FieldDoubleAssignment", priority, e, DUPLICATE_ASSIGNMENT_AT.create(mc
+                                    .getLocation(e2)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ huntbugs/src/test/java/one/util/huntbugs/testdata/TestDuplicateAssignment.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2016 HuntBugs contributors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package one.util.huntbugs.testdata;
+
+import one.util.huntbugs.registry.anno.AssertNoWarning;
+import one.util.huntbugs.registry.anno.AssertWarning;
+
+/**
+ * @author lan
+ *
+ */
+public class TestDuplicateAssignment {
+    int x = 1, y;
+    
+    @AssertNoWarning(type="FieldDoubleAssignment")
+    public TestDuplicateAssignment(int a) {
+        x = a;
+    }
+    
+    @SuppressWarnings("cast")
+    @AssertWarning(type="FieldDoubleAssignment")
+    public void doubleFieldChain(int val) {
+        this.x = (int)(this.x = Math.abs(val));
+    }
+    
+    @AssertWarning(type="FieldDoubleAssignment")
+    public void doubleField(int val) {
+        this.x = Math.abs(val);
+        this.x = Math.addExact(val, val);
+    }
+    
+    @AssertNoWarning(type="FieldDoubleAssignment")
+    public void doubleDiffField(int val) {
+        this.x = Math.abs(val);
+        this.y = Math.addExact(val, val);
+    }
+    
+    @AssertWarning(type="FieldDoubleAssignment")
+    public void doubleDiffFieldSameField(int val) {
+        this.x = Math.abs(val);
+        this.y = Math.addExact(val, val);
+        this.x = Math.decrementExact(val);
+    }
+    
+    @AssertWarning(type="FieldDoubleAssignment")
+    public void array(TestDuplicateAssignment[] arr, int val) {
+        arr[val].x = val;
+        arr[val].x = val;
+    }
+
+    @AssertNoWarning(type="FieldDoubleAssignment")
+    public void arrayOk(TestDuplicateAssignment[] arr, int val) {
+        arr[0].x = val;
+        arr[1].x = val;
+    }
+    
+    @AssertNoWarning(type="FieldDoubleAssignment")
+    public void doubleDiffObject(TestDuplicateAssignment tda, int val) {
+        this.x = Math.abs(val);
+        tda.x = Math.addExact(val, val);
+    }
+}
